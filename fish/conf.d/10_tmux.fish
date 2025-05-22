7ade0a41b64288f7ab391cdb4ab34c042f216fd5
Tim Paduraru <timopaduraru@gmail.com>
2025-05-22 04:20:35 -0700

    initcial commit

---
+++ fish/conf.d/10_tmux.fish
@@ -0,0 +1,9 @@
+#!/bin/usr/env fish
+# Configures a default tmux session
+
+# Only start tmux if we're on a real terminal and not already inside tmux
+if status is-interactive; and not set -q TMUX 
+    tmux has-session -t $hostname ^/dev/null
+    or tmux new-session -d -s $hostname
+end
+
---
+++ fish/functions/fish/config.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
