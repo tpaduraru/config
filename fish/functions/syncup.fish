f373b74dbaafdb8f6f9647c03305003eb1587d72
Tim Paduraru <timopaduraru@gmail.com>
2025-06-04 16:52:18 -0700

    poppy: fixed commit arguments

--- fish/functions/syncup.fish
+++ fish/functions/syncup.fish
@@ -1,6 +1,6 @@
 function syncup
-cd ~/.config
-git add fish tmux nvim
-git commit -m "$hostname: $1"
-git push
+    cd ~/.config
+    git add fish tmux nvim
+    git commit -m "$hostname: $argv[1]"
+    git push
 end
